038b20abc198ed523585b64648df77cca17d78fb
ronniekram <arynn.boniface@gmail.com>
2023-06-15 10:31:02 -0400

    fix: gameplay intro

--- src/slides/003-intro.tsx
+++ src/slides/003-intro.tsx
@@ -3,16 +3,17 @@ import tw, { styled } from "twin.macro";
 //! ----------> STYLES <----------
 export const Wrapper = styled.section`
   ${tw`w-full max-h-fit flex flex-col items-center mx-auto`};
+  ${tw`lg:(w-[80%]) xl:(w-[75%]) max-w-[61.875rem]`};
   ${tw`space-y-8`};
   ${tw`px-[5.3%] pt-10 pb-16 md:(px-[5.2%]) xl:(pt-14) 2xl:(px-0)`};
 `;
 
 const Video = styled.div`
-  ${tw`mx-auto w-full h-[fit-content] lg:(w-[80%]) xl:(w-[75%])`};
+  ${tw`mx-auto w-full h-[fit-content]`};
   ${tw`flex`};
 `;
 
-const P = tw.p`mx-auto font-sans font-bold text-white text-xl md:(text-2xl)  lg:(text-3xl) xl:(text-4xl) h-[fit-content] lg:(w-[80%] xl:(w-[75%]))`;
+const P = tw.p`mx-auto font-sans font-bold text-white text-xl md:(text-2xl)  lg:(text-3xl) xl:(text-4xl) h-[fit-content]`;
 
 //! ----------> COMPONENTS <----------
 const GameplayIntro = () => {
